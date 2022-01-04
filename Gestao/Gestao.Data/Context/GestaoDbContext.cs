f49ab45768ae43066ef29c90fdf0a2e698e4d274
Michael Peter <michael_piterct@hotmail.com>
2022-01-03 21:27:02 -0300

    GestaoDbContext

---
+++ Gestao/Gestao.Data/Context/GestaoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Gestao.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Data.Context
+{
+    public class GestaoDbContext: DbContext
+    {
+        public GestaoDbContext(DbContextOptions options) : base(options)   { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
