bd143c578d5ade955fe481c7fc50e2c934edea83
Michael Peter <michael_piterct@hotmail.com>
2022-01-06 08:12:11 -0300

    ProdutoRepository

---
+++ Gestao/Gestao.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,30 @@
+﻿using Gestao.Business.Interfaces;
+using Gestao.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public async Task<Produto> ObterProdutosFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking().Include(f => f.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking().Include(f => f.Fornecedor)
+                .OrderBy(p => p.Nome).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
