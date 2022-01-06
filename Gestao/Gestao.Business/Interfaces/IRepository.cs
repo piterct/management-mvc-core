3b381c24943a2a515aa9d7a9a2539df7f9547a87
Michael Peter <michael_piterct@hotmail.com>
2022-01-06 07:29:39 -0300

    IRepository

---
+++ Gestao/Gestao.Business/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gestao.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gestao.Business.Interfaces
+{
+    public  interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity obj);
+        Task Remover(Guid id);
+    }
+}
