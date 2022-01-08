0197b88f37a27801912538413ecf0d8a1797bfa8
Michael Peter <michael_piterct@hotmail.com>
2022-01-08 15:34:33 -0300

    layout create new produto

--- Gestao/Gestao.App/ViewModels/ProdutoViewModel.cs
+++ Gestao/Gestao.App/ViewModels/ProdutoViewModel.cs
@@ -25,11 +25,13 @@ namespace Gestao.App.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
+
         [ScaffoldColumn(false)]
         public DateTime DataCadastro { get; set; }
 
