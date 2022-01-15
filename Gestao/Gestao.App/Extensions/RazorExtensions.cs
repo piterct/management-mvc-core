7f9c581dca30fa2df47ea4eb15178c2c594b9779
Michael Peter <michael_piterct@hotmail.com>
2022-01-14 23:57:57 -0300

    validate special caracters

--- Gestao/Gestao.App/Extensions/RazorExtensions.cs
+++ Gestao/Gestao.App/Extensions/RazorExtensions.cs
@@ -7,6 +7,7 @@ namespace Gestao.App.Extensions
     {
         public static string FormataDocumento(this RazorPage page, int tipoPessoa, string documento)
         {
+            documento = documento.Replace(".", "").Replace("-", "").Replace("/", "").Trim();
             return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
         }
     }
