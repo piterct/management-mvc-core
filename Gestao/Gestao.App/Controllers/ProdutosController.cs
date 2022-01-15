afbb9f5081875414f4ad6b553f04d3a1c45f7c3e
Michael Peter <michael_piterct@hotmail.com>
2022-01-15 13:52:25 -0300

    produtos routes

--- Gestao/Gestao.App/Controllers/ProdutosController.cs
+++ Gestao/Gestao.App/Controllers/ProdutosController.cs
@@ -26,11 +26,13 @@ namespace Gestao.App.Controllers
             _fornecedorRepository = fornecedorRepository;
         }
 
+        [Route("lista-de-produtos")]
         public async Task<IActionResult> Index()
         {
             return View(_mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores()));
         }
 
+        [Route("dados-do-produto/{id:guid}")]
         public async Task<IActionResult> Details(Guid id)
         {
             var produtoViewModel = await ObterProduto(id);
@@ -43,7 +45,7 @@ namespace Gestao.App.Controllers
             return View(produtoViewModel);
         }
 
-
+        [Route("novo-produto")]
         public async Task<IActionResult> Create()
         {
             var produtoViewModel = await PopularFornecedores(new ProdutoViewModel());
@@ -51,6 +53,7 @@ namespace Gestao.App.Controllers
             return View(produtoViewModel);
         }
 
+        [Route("novo-produto")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProdutoViewModel produtoViewModel)
@@ -70,6 +73,8 @@ namespace Gestao.App.Controllers
 
             return RedirectToAction("Index");
         }
+
+        [Route("editar-produto/{id:guid}")]
         public async Task<IActionResult> Edit(Guid id)
         {
             var produtoViewModel = await ObterProduto(id);
@@ -82,6 +87,7 @@ namespace Gestao.App.Controllers
             return View(produtoViewModel);
         }
 
+        [Route("editar-produto/{id:guid}")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Guid id, ProdutoViewModel produtoViewModel)
@@ -115,7 +121,7 @@ namespace Gestao.App.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-
+        [Route("excluir-produto/{id:guid}")]
         public async Task<IActionResult> Delete(Guid id)
         {
             var produto = await ObterProduto(id);
@@ -128,7 +134,7 @@ namespace Gestao.App.Controllers
             return View(produto);
         }
 
-
+        [Route("excluir-produto/{id:guid}")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
