83caf979e5ffe25e1cd50329404ae878a57dfe5a
Michael Peter <michael_piterct@hotmail.com>
2022-01-14 23:20:25 -0300

    FormataDocumento

--- Gestao/Gestao.App/Extensions/RazorExtensions.cs
+++ Gestao/Gestao.App/Extensions/RazorExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace Gestao.App.Extensions
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace Gestao.App.Extensions
 {
-    public class RazorExtensions
+    public static class RazorExtensions
     {
+        public static string FormataDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
     }
 }
