9185a50bd5fb05de46170bb579a075bbad86583d
Michael Peter <michael_piterct@hotmail.com>
2022-03-19 10:37:45 -0300

    NotificarErroCommandInvalido

--- Gestao/Gestao.App/Controllers/BaseController.cs
+++ Gestao/Gestao.App/Controllers/BaseController.cs
@@ -1,5 +1,9 @@
-﻿using Gestao.Business.Interfaces;
+﻿using Flunt.Notifications;
+using Gestao.Business.Commands.Shared;
+using Gestao.Business.Interfaces;
+using Gestao.Business.Notificacoes;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Gestao.App.Controllers
 {
@@ -15,6 +19,25 @@ namespace Gestao.App.Controllers
         protected bool OperacaoValida()
         {
             return !_notificador.TemNotificacao();
+        }
+
+        protected bool ValidacaoCommand<TE>(TE entidade) where TE : CommandEntity
+        {
+            entidade.Validate();
+            if (entidade.Valid) return true;
+
+            NotificarErroCommandInvalido(entidade.Notifications);
+
+            return false;
+
+        }
+
+        protected void NotificarErroCommandInvalido(IReadOnlyCollection<Notification> notificacoes)
+        {
+            foreach(var erro in notificacoes)
+            {
+                _notificador.Handle(new Notificacao(erro.Message));
+            }
         }
     }
 }
