08e2e6f0e54cf09d5cb95df0d127f340c6d6cf7b
Michael Peter <michael_piterct@hotmail.com>
2022-03-19 10:16:04 -0300

    CriaFornecedorCommand

---
+++ Gestao/Gestao.Business/Commands/Input/CriaFornecedorCommand.cs
@@ -0,0 +1,18 @@
+﻿using Gestao.Business.Models;
+using System.Collections.Generic;
+
+namespace Gestao.Business.Commands.Input
+{
+    public  class CriaFornecedorCommand
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* Ef Relations */
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
