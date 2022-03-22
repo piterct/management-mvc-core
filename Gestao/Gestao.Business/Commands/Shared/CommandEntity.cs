70ee951a8e0178d40cb9de5703d90118583a0826
Michael Peter <michael_piterct@hotmail.com>
2022-03-21 23:33:36 -0300

    CommandId

--- Gestao/Gestao.Business/Commands/Shared/CommandEntity.cs
+++ Gestao/Gestao.Business/Commands/Shared/CommandEntity.cs
@@ -1,10 +1,16 @@
 ﻿using Flunt.Notifications;
 using Gestao.Business.Commands.Interfaces;
+using System;
 
 namespace Gestao.Business.Commands.Shared
 {
     public abstract  class CommandEntity :  Notifiable, ICommand
     {
+        public CommandEntity()
+        {
+            CommandId = Guid.NewGuid();
+        }
+        public Guid CommandId { get; set; }
         public abstract void Validate();
     }
 }
