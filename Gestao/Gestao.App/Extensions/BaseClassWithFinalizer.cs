431043f5b6951b955f6e8a7c9f747b5173ec3f80
Michael Peter <michael_piterct@hotmail.com>
2022-06-12 01:29:38 -0300

    BaseClassWithFinalizer

---
+++ Gestao/Gestao.App/Extensions/BaseClassWithFinalizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Gestao.App.Extensions
+{
+    public class BaseClassWithFinalizer : IDisposable
+    {
+        // To detect redundant calls
+        private bool _disposedValue;
+
+        ~BaseClassWithFinalizer() => Dispose(false);
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposedValue = true;
+            }
+        }
+    }
+}
