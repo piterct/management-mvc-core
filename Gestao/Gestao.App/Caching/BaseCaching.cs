7319f668397a253eb3057df70ee7b4e12dc3b684
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-07-11 01:39:16 -0300

    BaseCaching

---
+++ Gestao/Gestao.App/Caching/BaseCaching.cs
@@ -0,0 +1,13 @@
+﻿using Gestao.Business.Interfaces;
+
+namespace Gestao.App.Caching
+{
+    public abstract class BaseCaching<T> where T : class
+    {
+        protected string GetKey(string key)
+        {
+            return $"{typeof(T).FullName}:{key}";
+        }
+
+    }
+}
