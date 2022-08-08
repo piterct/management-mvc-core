9060ee398d2cf84594733362a83c13048ca420a8
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-08 00:07:28 -0300

    Fornecedor query

---
+++ Gestao/Gestao.Business/Queries/Fornecedor/FornecedorResult.cs
@@ -0,0 +1,14 @@
+﻿using Gestao.Business.Models;
+using System;
+
+namespace Gestao.Business.Queries.Fornecedor
+{
+    public class FornecedorResult
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ Gestao/Gestao.Business/Queries/Fornecedor/IFornecedorQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gestao.Business.Queries.Fornecedor
+{
+    public interface IFornecedorQuery: IQuery
+    {
+        Task<IEnumerable<FornecedorResult>> ObterTodos();
+        Task<IEnumerable<FornecedorResult>> ObterTodosProcedure();
+    }
+}
