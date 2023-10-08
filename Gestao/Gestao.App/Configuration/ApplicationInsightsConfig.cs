c33b2411cd08bdd52ec567984f632e6172bfe2ea
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-10-08 17:42:06 -0300

    ApplicationInsightsConfig Class

---
+++ Gestao/Gestao.App/Configuration/ApplicationInsightsConfig.cs
@@ -0,0 +1,17 @@
+﻿using Gestao.App.Middlewares;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gestao.App.Configuration
+{
+    public static class ApplicationInsightsConfig
+    {
+
+        public static IServiceCollection AddApplicationInsightConfig(this IServiceCollection services)
+        {
+            services.AddTransient<RequestBodyLoggingMiddleware>();
+            services.AddTransient<ResponseBodyLoggingMiddleware>();
+
+            return services;
+        }
+    }
+}
